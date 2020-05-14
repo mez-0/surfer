db372f525d7517c43dcc98e07fa5e0447083a38f
mez-0 <mez0@pm.me>
2020-05-14 12:22:17 +0100

    added help page

--- lib/cli.py
+++ lib/cli.py
@@ -6,6 +6,22 @@ from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.styles import Style
 from prompt_toolkit.completion import NestedCompleter
 
+def start_cli():
+	session = PromptSession()
+	try:
+		while True:
+			cmd = session.prompt(prompt, style=style, completer=completer)
+			if 'help' in cmd:
+				show_help()
+			else:
+				jobs.do(cmd)
+	except KeyboardInterrupt as e:
+		quit()
+
+def show_help():
+	for c,h in helper.items():
+		print('%-9s %s' % (c, logger.blue(h)))
+
 style = Style.from_dict({
     # User input (default text).
     '':          '#ffffff',
@@ -25,12 +41,10 @@ completer = NestedCompleter.from_nested_dict({
     'generate': {'curl','wget','Invoke-WebRequest','DownloadString'}
 })
 
-def start_cli():
-	session = PromptSession()
-	try:
-		while True:
-			cmd = session.prompt(prompt, style=style, completer=completer)
-			jobs.do(cmd)
-	except KeyboardInterrupt as e:
-		quit()
-
+helper = {
+	'list':'List the contents of the server directory',
+	'generate':'Generate download utility commands',
+	'add':'Add a file to the server directory',
+	'delete':'Delete a file from the server directory',
+	'exit':'Close Surfer.py'
+}
